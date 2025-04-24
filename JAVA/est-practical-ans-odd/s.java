d400fd39627415bc1d8a929af77eff9ffd072792
sumit-616 <sumit616794@gmail.com>
2025-04-24 11:58:46 +0530

    Create s.java

---
+++ JAVA/est-practical-ans-odd/s.java
@@ -0,0 +1,39 @@
+import java.io.*;
+
+class Student implements Serializable {
+    String name;
+    int age;
+    transient double marks;
+
+    public Student(String name, int age, double marks) {
+        this.name = name;
+        this.age = age;
+        this.marks = marks;
+    }
+}
+
+public 
+
+
+public class SerializeDemo {
+    public static void main(String[] args) {
+        Student s1 = new Student("John", 20, 92.5);
+
+        try (ObjectOutputStream out = new ObjectOutputStream(new FileOutputStream("student.ser"))) {
+            out.writeObject(s1);
+            System.out.println("Object has been serialized.");
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+
+        try (ObjectInputStream in = new ObjectInputStream(new FileInputStream("student.ser"))) {
+            Student s2 = (Student) in.readObject();
+            System.out.println("Object has been deserialized.");
+            System.out.println("Name: " + s2.name);
+            System.out.println("Age: " + s2.age);
+            System.out.println("Marks (transient): " + s2.marks);
+        } catch (IOException | ClassNotFoundException e) {
+            e.printStackTrace();
+        }
+    }
+}
